dcfcf6003dc6957c23964fe07974dfd058375c66
virginiasatyro <vivi_satyro@hotmail.com>
2018-12-11 23:22:11 -0200

    tentativa final

---
+++ PARTE II - Tipos e Operacoes/CAP 4 - Numeros/conceitos_numeros.py
@@ -0,0 +1,35 @@
+# ESTRUTURA DE PROGRAMA EM PYTHON
+# 1. Os programas são compostos de módulos;
+# 2. Os módulos contêm instruções;
+# 3. As instruções contêm expressões;
+# 4. As expressões cria, e processam objetos;
+
+# * Os objetos incorporados facilitam a escrita de programas simples;
+# * O Python fornece objetos e suporta extensões;
+# * Os objetos incorporados são componentes de extensões;
+# * Frequentemente, os objetos incorporados são mais eficientes do que as estruturas de dados personalizadas;
+
+'''
+TIPO DE OBJETO  EXEMPLO DE LITERAIS
+Números         3.1415, 1234, 999L, 3 + 4j
+Strings         'spam', "guido's"
+Listas          {1, [2, 'three'], 4}
+Dicionários  	{'food' : 'spam' : 'yum'}
+Tuplas          (1, 'spam', 4, 'U')
+Arquivos        text = open('eggs', 'r').read()
+'''
+
+'''
+LITERAL                      INTERPRETAÇÃO
+1234, -24, 0				 Inteiros normais (long em C)
+999999999999L                Inteiros longos (tamanho ilimitado)
+1.23, 3.14e-10, 4E210	     Ponto flutuante (double em C)
+0177, 0x9ff, 0xFF            Literais em octal e hexadecimal
+3 + 4j, 3.0 + 4.0j, 3J       Literais numéricas complexas
+'''
+
+# Operandos de expressão: +, -, >>, **etc.
+# Funções matemáticas internas: pow, abs, etc.
+# Módulos utilitários: random, math, etc.
+
+# NUMPY: 
---
+++ hello.py
@@ -0,0 +1,5 @@
+# Passo 1: instalar python
+# Passo 2: achar arquio .py no PC
+# Passo 3: python hello.py
+
+print ("Hello World!")
