da21e2c4a5fdeafc8c39948310ec848eb207bad8
virginiasatyro <vivi_satyro@hotmail.com>
2019-02-06 11:17:19 -0200

    introducao a POO - CAP 19 - OK

--- PARTE V - Modulos/CAP 16 - Fundamentos Desenvolvimento Modulos/desenvolvimento_modulos.py
+++ PARTE V - Modulos/CAP 16 - Fundamentos Desenvolvimento Modulos/desenvolvimento_modulos.py
@@ -37,3 +37,33 @@ print(simple.spam) # o código não foi executado novamente: atributo inalterado
 # = 2
 
 # IMPORT e FROM são atribuições
+# * IMPORT - atribui a um objeto módulo inteiro a um único nome;
+# * FROM - atribui um ou mais nomes a objetos de mesmo nome em outro módulo;
+from small import x, y # copia 2 nomes
+print(x)    # 1 
+print(y[0]) # 1 
+x = 42
+print(x)    # 42 
+print(y[0]) # seria 42 em versões anteriores do Python, para a versão 3, permanece 1
+''' 
+Aparentemente algumas regras do capítulo não se aplicam a Python 3,
+pularei alguns exemplos.
+'''
+
+import small
+print(small.y) # [1, 2]
+
+'''
+from module import name1, name2    # copia esses dois nomes (apenas)
+
+import module           # busca o objeto módulo
+name1 = module.name1    # copia nomes pela atribuição
+name2 = module.name2
+del module              # desfaz-se do nome do módulo
+'''
+
+
+# ESPAÇOS DE NOME DE MÓDULO
+
+
+
