d7b0621b317fa486adf567d29c6c21863c21e123
virginiasatyro <vivi_satyro@hotmail.com>
2019-02-06 12:34:10 -0200

    Modulos - CAP 16 - OK

---
+++ PARTE V - Modulos/CAP 16 - Fundamentos Desenvolvimento Modulos/mod1.py
@@ -0,0 +1,6 @@
+x = 1
+import mod2 # = 2 3 
+
+print (x)            # meu x global        = 1
+print (mod2.x)       # x de mod2           = 2
+print (mod2.mod3.x)  # x de mod3 aninhado  = 3
---
+++ PARTE V - Modulos/CAP 16 - Fundamentos Desenvolvimento Modulos/mod2.py
@@ -0,0 +1,5 @@
+x = 2
+import mod3
+
+print(x)       # meu x global = 2 
+print(mod3.x)  # x de mod3    = 3
