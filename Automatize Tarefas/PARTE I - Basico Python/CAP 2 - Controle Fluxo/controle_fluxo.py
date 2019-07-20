de23e49f66e37103211dd1de180d055559291685
virginiasatyro <vivi_satyro@hotmail.com>
2019-07-19 23:16:52 -0300

    CAP 3 - Funcoes - OK

--- Automatize Tarefas/PARTE I - Basico Python/CAP 2 - Controle Fluxo/controle_fluxo.py
+++ Automatize Tarefas/PARTE I - Basico Python/CAP 2 - Controle Fluxo/controle_fluxo.py
@@ -50,3 +50,38 @@ while True:
 		break;
 		
 print('Thank you!')
+
+
+print('My name is ')
+for i in range(5):
+	print('Jimmy Five times (' + str(i) + ')') # 0 1 2 3 4
+	
+# Gauss
+total = 0
+for num in range(101):
+	total = total + num
+print(total)
+
+for i in range(12, 16):
+	print(i) # Output: 12 13 14 15
+	
+for i in range(0, 10, 2):
+	print(i) # Output: 0 2 4 6 8 
+	
+for i in range(5, -1, -1):
+	print(i) # Output: 5 4 3 2 1 0
+	
+# IMPORTANDO MÓDULOS
+import random 
+for i in range(5): # randint - valor inteiro aleatório entre dois inteiros passados a ela 
+	print(random.randint(1, 10)) # Output: 5 1 6 2 1 (random)
+	
+# SYS.EXIT() - programa é encerrado
+import sys
+
+while True:
+	print('\nType exit to exit.')
+	response = input()
+	if response == 'exit':
+		sys.exit()
+	print('You typed ' + response + ' .')
